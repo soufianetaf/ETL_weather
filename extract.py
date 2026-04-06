dfd625559129847ab5c0c5c980183919cac97009
soufianetaf <itssoufianetafahi@gmail.com>
2026-04-06 20:10:48 +0200

    Extraction des données depuis openweathermap

---
+++ extract.py
@@ -0,0 +1,15 @@
+import requests
+
+def extract_weather_data(city, api_key):
+    " recupere les données météo brutes depuis l'API OpenWeatherMap. "
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}appid={api_key}&units=metric"
+
+
+    try :
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+
+    except requests.exceptions.RequestException as e:
+        print(f"Erreur d'ectraction : {e}")
+        return None
